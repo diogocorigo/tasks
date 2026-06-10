393a6c191d3545900511c66f9f722291b1cd6421
diogocorigo <diogoacorigo@gmail.com>
2026-06-10 23:08:46 +0100

    fixed landing page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,8 +3,8 @@ import { FlameIcon } from "lucide-react";
 export default function App() {
   return (
         <div className="w-screen h-screen flex items-center justify-center">
-            <h1 className="text-3xl font-bold flex items-center gap-4">
-                <FlameIcon size={64} />
+            <h1 className="text-3xl font-bold flex items-center gap-2">
+                <FlameIcon size={32} />
                 <span>Tasks</span>
             </h1>
         </div>
