e62d193c33efb1a8cab49af470150937f796d5e6
diogocorigo <diogoacorigo@gmail.com>
2026-06-10 21:55:58 +0100

    added webview package and integrated index.html

--- main.go
+++ main.go
@@ -1,7 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"embed"
+	"fmt"
+	"io/fs"
+	"net"
+	"net/http"
+	"os"
+
+	webview "github.com/webview/webview_go"
+)
+
+//go:embed frontend/*
+var frontendFiles embed.FS
 
 func main() {
-	fmt.Println("Hello World!")
+	port := createLocalServer()
+
+	w := webview.New(true)
+	defer w.Destroy()
+
+	w.SetTitle("Tasks")
+	w.SetSize(1280, 720, 0)
+
+	w.Bind("hello", func() string {
+		return "Hello from Webview!"
+	})
+
+	w.Navigate(fmt.Sprintf("http://localhost:%d/index.html", port))
+	w.Run()
+}
+
+func createLocalServer() int {
+	subFS, err := fs.Sub(frontendFiles, "frontend")
+	if err != nil {
+		panic(err)
+	}
+
+	http.Handle("/", http.FileServer(http.FS(subFS)))
+
+	// get a free port
+	ln, err := net.Listen("tcp", ":0")
+	if err != nil {
+		panic(err)
+	}
+	port := ln.Addr().(*net.TCPAddr).Port
+	ln.Close()
+
+	server := &http.Server{Addr: fmt.Sprintf(":%d", port)}
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			fmt.Printf("Server error: %s\n", err)
+			os.Exit(1)
+		}
+	}()
+
+	return port
 }
